626395ca92c9462a39fa0b6ddebefe4a2bd661bf
sssoft <sergey_me@mail.ru>
2016-12-06 12:20:22 +0200

    Implement Add and Remove note

--- public/notes.js
+++ public/notes.js
@@ -7,13 +7,28 @@ angular.module('app', []);
         var self = this;
         self.notes = [];
 
-        var update = function () {
+        var update = function() {
             $http.get("/notes")
                 .success(function (notes) {
                     self.notes = notes;
                 })
         };
-
         update();
+
+        self.add = function () {
+            var note = {text: self.text};
+            $http.post("/notes", note)
+                .success(function () {
+                    self.text = "";
+                    update();
+                });
+        };
+
+        self.remove = function(id) {
+            $http.delete("/notes", {params: {'id' : id}})
+                .success(function () {
+                    update();
+                });
+        };
     }
 })();
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var session = require('express-session');
+var bodyParser = require('body-parser');
+
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'angular_notes',
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.listen(3000);
 
@@ -10,10 +21,33 @@ app.get("/greeting", function (req, res) {
 });
 
 app.get("/notes", function (req, res) {
-    var notes = [
-        {text: "1. First note"},
-        {text: "2. Second note"},
-        {text: "3. Third note"}
-    ];
-    res.send(notes);
+    res.send(req.session.notes || []);
+});
+
+app.post("/notes", function (req, res) {
+   if (!req.session.notes) {
+       req.session.notes = [];
+       req.session.last_note_id = 0;
+   }
+   var note = req.body;
+   note.id = req.session.last_note_id;
+   console.log(note.id + ". " + note);
+   req.session.last_note_id++;
+   req.session.notes.push(note);
+   res.end();
+});
+
+app.delete("/notes", function (req, res) {
+    var id = req.query.id;
+    var notes = req.session.notes || [];
+    var updatedNotesList = [];
+
+    for (var i = 0; i < notes.length; i++) {
+        if (notes[i].id != id) {
+            updatedNotesList.push(notes[i]);
+        }
+    }
+
+    req.session.notes = updatedNotesList;
+    res.end();
 })
